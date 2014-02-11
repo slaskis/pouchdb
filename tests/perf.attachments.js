a86f825ba3426497788092045ed10ad182fdcde7
Robert Sköld <robert@publicclass.se>
2014-02-11 16:49:46 +0100

    Updated perf attachments to use mocha bdd ui

--- tests/perf.attachments.js
+++ tests/perf.attachments.js
@@ -2,164 +2,183 @@
 
 var adapters = ["local-1"];
 
-if (typeof module !== 'undefined' && module.exports) {
-  var PouchDB = require('../lib');
-  var testUtils = require('./test.utils.js');
-}
+var testHelpers = {};
+describe('performance',function(){
+  describe('attachments',function(){
+    adapters.map(function (adapter) {
+      describe(adapter, function(){
+        var db;
+        beforeEach(function (done) {
+          var name = testUtils.generateAdapterUrl(adapter);
+          testUtils.initTestDB(name,function(err,database){
+            db = database;
+            done(err);
+          });
+        });
+        afterEach(testUtils.cleanupTestDatabases);
 
+        var large = 5 * 1024 * 1024;
 
-adapters.map(function (adapter) {
+        var largeString = "";
+        for(var i=0; i<large; i++){
+          largeString += 'a';
+        }
 
-  QUnit.module('attachment performance: ' + adapter, {
-    setup : function () {
-      this.name = testUtils.generateAdapterUrl(adapter);
-    },
-    teardown: testUtils.cleanupTestDatabases
-  });
+        var largeBuffer = new Buffer(large);
+        for(var i=0; i<large; i++){
+          largeBuffer[i] = (0xFF * Math.random()) | 0;
+        }
 
-  var large = 5 * 1024 * 1024;
+        function map(doc) {
+          /* globals emit */
+          emit(null, doc);
+        }
 
-  var largeString = "";
-  for(var i=0; i<large; i++){
-    largeString += 'a';
-  }
-
-  var largeBuffer = new Buffer(large);
-  for(var i=0; i<large; i++){
-    largeBuffer[i] = (0xFF * Math.random()) | 0;
-  }
-
-  function map(doc) {
-    /* globals emit */
-    emit(null, doc);
-  }
-
-  asyncTest("Test large string attachments", function (done) {
-    testUtils.initTestDB(this.name, function (err, db) {
-      var queryStartTime1 = new Date().getTime();
-      db.query({map: map}, function (err, response) {
-        var queryDuration1 = new Date().getTime() - queryStartTime1;
-        db.put({ _id: 'doc1' }, function (err, resp) {
-          var writeStartTime1 = Date.now();
-          db.putAttachment('doc1', 'string1', resp.rev, largeString, 'text/plain', function (err, res) {
-            var writeDuration1 = Date.now() - writeStartTime1;
-            db.put({ _id: 'doc2' }, function (err, resp) {
-              var writeStartTime2 = Date.now();
-              db.putAttachment('doc2','string2', resp.rev, largeString, 'text/plain', function (err, res) {
-                var writeDuration2 = Date.now() - writeStartTime2;
-                var queryStartTime2 = new Date().getTime();
-                db.query({map: map}, function (err, response) {
-                  var queryDuration2 = new Date().getTime() - queryStartTime2;
-                  console.log("write doc1/string1 took " + writeDuration1 + " ms");
-                  console.log("write doc2/string2 took " + writeDuration2 + " ms");
-                  console.log("query before attachments took " + queryDuration1 + " ms");
-                  console.log("query after attachments took " + queryDuration2 + " ms");
-                  ok(queryDuration2 <= queryDuration1 * 10, 'Query finished within order of magnitude');
-                  done();
+        it("large string attachments", function (done) {
+          var queryStartTime1 = Date.now();
+          db.query({map: map}, function (err, response) {
+            if( err ){ return done(err) }
+            var queryDuration1 = Date.now() - queryStartTime1;
+            db.put({ _id: 'doc1' }, function (err, resp) {
+              if( err ){ return done(err) }
+              var writeStartTime1 = Date.now();
+              db.putAttachment('doc1', 'string1', resp.rev, largeString, 'text/plain', function (err, res) {
+                if( err ){ return done(err) }
+                var writeDuration1 = Date.now() - writeStartTime1;
+                db.put({ _id: 'doc2' }, function (err, resp) {
+                  if( err ){ return done(err) }
+                  var writeStartTime2 = Date.now();
+                  db.putAttachment('doc2','string2', resp.rev, largeString, 'text/plain', function (err, res) {
+                    if( err ){ return done(err) }
+                    var writeDuration2 = Date.now() - writeStartTime2;
+                    var queryStartTime2 = Date.now();
+                    db.query({map: map}, function (err, response) {
+                      if( err ){ return done(err) }
+                      var queryDuration2 = Date.now() - queryStartTime2;
+                      console.log("write doc1/string1 took " + writeDuration1 + " ms");
+                      console.log("write doc2/string2 took " + writeDuration2 + " ms");
+                      console.log("query before attachments took " + queryDuration1 + " ms");
+                      console.log("query after attachments took " + queryDuration2 + " ms");
+                      // try {
+                        queryDuration2.should.be.below(0);
+                      // } catch(e){ return done(e) }
+                      done();
+                    });
+                  });
                 });
               });
             });
           });
         });
-      });
-    });
-  });
 
-  asyncTest("Test large buffer attachments", function (done) {
-    testUtils.initTestDB(this.name, function (err, db) {
-      var queryStartTime1 = new Date().getTime();
-      db.query({map: map}, function (err, response) {
-        var queryDuration1 = new Date().getTime() - queryStartTime1;
-        db.put({ _id: 'doc1' }, function (err, resp) {
-          var writeStartTime1 = Date.now();
-          db.putAttachment('doc1', 'buffer1', resp.rev, largeBuffer, 'application/octet-stream', function (err, res) {
-            var writeDuration1 = Date.now() - writeStartTime1;
-            db.put({ _id: 'doc2' }, function (err, resp) {
-              var writeStartTime2 = Date.now();
-              db.putAttachment('doc2','buffer2', resp.rev, largeBuffer, 'application/octet-stream', function (err, res) {
-                var writeDuration2 = Date.now() - writeStartTime2;
-                var queryStartTime2 = new Date().getTime();
-                db.query({map: map},function (err, response) {
-                  var queryDuration2 = new Date().getTime() - queryStartTime2;
-                  console.log("write doc1/buffer1 took " + writeDuration1 + " ms");
-                  console.log("write doc2/buffer2 took " + writeDuration2 + " ms");
-                  console.log("query before attachments took " + queryDuration1 + " ms");
-                  console.log("query after attachments took " + queryDuration2 + " ms");
-                  ok(queryDuration2 <= queryDuration1 * 10, 'Query finished within order of magnitude');
-                  done();
+        it("large buffer attachments", function (done) {
+          var queryStartTime1 = Date.now();
+          db.query({map: map}, function (err, response) {
+            if( err ){ return done(err) }
+            var queryDuration1 = Date.now() - queryStartTime1;
+            db.put({ _id: 'doc1' }, function (err, resp) {
+              if( err ){ return done(err) }
+              var writeStartTime1 = Date.now();
+              db.putAttachment('doc1', 'buffer1', resp.rev, largeBuffer, 'application/octet-stream', function (err, res) {
+                if( err ){ return done(err) }
+                var writeDuration1 = Date.now() - writeStartTime1;
+                db.put({ _id: 'doc2' }, function (err, resp) {
+                  if( err ){ return done(err) }
+                  var writeStartTime2 = Date.now();
+                  db.putAttachment('doc2','buffer2', resp.rev, largeBuffer, 'application/octet-stream', function (err, res) {
+                    if( err ){ return done(err) }
+                    var writeDuration2 = Date.now() - writeStartTime2;
+                    var queryStartTime2 = Date.now();
+                    db.query({map: map},function (err, response) {
+                      if( err ){ return done(err) }
+                      var queryDuration2 = Date.now() - queryStartTime2;
+                      console.log("write doc1/buffer1 took " + writeDuration1 + " ms");
+                      console.log("write doc2/buffer2 took " + writeDuration2 + " ms");
+                      console.log("query before attachments took " + queryDuration1 + " ms");
+                      console.log("query after attachments took " + queryDuration2 + " ms");
+                      try {
+                        queryDuration2.should.be.below(Math.max(1,queryDuration1 * 10));
+                      } catch(e){ return done(e) }
+                      done();
+                    });
+                  });
                 });
               });
             });
           });
         });
-      });
-    });
-  });
 
-  asyncTest("Test large string and buffer attachments", function (done) {
-    testUtils.initTestDB(this.name, function (err, db) {
-      var queryStartTime1 = new Date().getTime();
-      db.query({map: map}, function (err, response) {
-        var queryDuration1 = new Date().getTime() - queryStartTime1;
-        db.put({ _id: 'doc1' }, function (err, resp) {
-          var writeStartTime1 = Date.now();
-          db.putAttachment('doc1', 'string1', resp.rev, largeString, 'text/plain', function (err, res) {
-            var writeDuration1 = Date.now() - writeStartTime1;
-            db.put({ _id: 'doc2' }, function (err, resp) {
-              var writeStartTime2 = Date.now();
-              db.putAttachment('doc2','buffer1', resp.rev, largeBuffer, 'application/octet-stream', function (err, res) {
-                var writeDuration2 = Date.now() - writeStartTime2;
-                var queryStartTime2 = new Date().getTime();
-                db.query({map: map},function (err, response) {
-                  var queryDuration2 = new Date().getTime() - queryStartTime2;
-                  console.log("write doc1/string1 took " + writeDuration1 + " ms");
-                  console.log("write doc2/buffer1 took " + writeDuration2 + " ms");
-                  console.log("query before attachments took " + queryDuration1 + " ms");
-                  console.log("query after attachments took " + queryDuration2 + " ms");
-                  ok(queryDuration2 <= queryDuration1 * 10, 'Query finished within order of magnitude');
-                  done();
+        it("large string and buffer attachments", function (done) {
+          var queryStartTime1 = Date.now();
+          db.query({map: map}, function (err, response) {
+            if( err ){ return done(err) }
+            var queryDuration1 = Date.now() - queryStartTime1;
+            db.put({ _id: 'doc1' }, function (err, resp) {
+              if( err ){ return done(err) }
+              var writeStartTime1 = Date.now();
+              db.putAttachment('doc1', 'string1', resp.rev, largeString, 'text/plain', function (err, res) {
+                if( err ){ return done(err) }
+                var writeDuration1 = Date.now() - writeStartTime1;
+                db.put({ _id: 'doc2' }, function (err, resp) {
+                  if( err ){ return done(err) }
+                  var writeStartTime2 = Date.now();
+                  db.putAttachment('doc2','buffer1', resp.rev, largeBuffer, 'application/octet-stream', function (err, res) {
+                    if( err ){ return done(err) }
+                    var writeDuration2 = Date.now() - writeStartTime2;
+                    var queryStartTime2 = Date.now();
+                    db.query({map: map},function (err, response) {
+                      if( err ){ return done(err) }
+                      var queryDuration2 = Date.now() - queryStartTime2;
+                      console.log("write doc1/string1 took " + writeDuration1 + " ms");
+                      console.log("write doc2/buffer1 took " + writeDuration2 + " ms");
+                      console.log("query before attachments took " + queryDuration1 + " ms");
+                      console.log("query after attachments took " + queryDuration2 + " ms");
+                      try {
+                        queryDuration2.should.be.below(Math.max(1,queryDuration1 * 10));
+                      } catch(e){ return done(e) }
+                      done();
+                    });
+                  });
                 });
               });
             });
           });
         });
-      });
+
+        it("large string and buffer attachments in a single doc", function (done) {
+          var queryStartTime1 = Date.now();
+          db.query({map: map}, function (err, response) {
+            if( err ){ return done(err) }
+            var queryDuration1 = Date.now() - queryStartTime1;
+            db.put({ _id: 'doc1' }, function (err, resp) {
+              if( err ){ return done(err) }
+              var writeStartTime1 = Date.now();
+              db.putAttachment('doc1', 'string1', resp.rev, largeString, 'text/plain', function (err, res) {
+                if( err ){ return done(err) }
+                var writeDuration1 = Date.now() - writeStartTime1;
+                var writeStartTime2 = Date.now();
+                db.putAttachment('doc1', 'buffer1', res.rev, largeBuffer, 'application/octet-stream', function (err, res) {
+                  if( err ){ return done(err) }
+                  var writeDuration2 = Date.now() - writeStartTime2;
+                  var queryStartTime2 = Date.now();
+                  db.query({map: map},function (err, response) {
+                    if( err ){ return done(err) }
+                    var queryDuration2 = Date.now() - queryStartTime2;
+                    console.log("write doc1/string1 took " + writeDuration1 + " ms");
+                    console.log("write doc1/buffer1 took " + writeDuration2 + " ms");
+                    console.log("query before attachments took " + queryDuration1 + " ms");
+                    console.log("query after attachments took " + queryDuration2 + " ms");
+                    try {
+                      queryDuration2.should.be.below(Math.max(1,queryDuration1 * 10));
+                    } catch(e){ return done(e) }
+                    done();
+                  });
+                });
+              });
+            });
+          });
+        });
+      })
     });
-  });
-
-  // Currently fails by timing out. Probably because of https://github.com/daleharvey/pouchdb/issues/1287
-  // asyncTest("Test large string and buffer attachments in one doc", function (done) {
-  //   testUtils.initTestDB(this.name, function (err, db) {
-  //     var queryStartTime1 = new Date().getTime();
-  //     db.query({map: map}, function (err, response) {
-  //       ok(!err,'no errors');
-  //       var queryDuration1 = new Date().getTime() - queryStartTime1;
-  //       db.put({ _id: 'doc1' }, function (err, resp) {
-  //         ok(!err,'no errors');
-  //         var writeStartTime1 = Date.now();
-  //         db.putAttachment('doc1', 'string1', resp.rev, largeString, 'text/plain', function (err, res) {
-  //           ok(!err,'no errors');
-  //           var writeDuration1 = Date.now() - writeStartTime1;
-  //           var writeStartTime2 = Date.now();
-  //           db.putAttachment('doc1', 'buffer1', res.rev, largeBuffer, 'application/octet-stream', function (err, res) {
-  //             ok(!err,'no errors');
-  //             var writeDuration2 = Date.now() - writeStartTime2;
-  //             var queryStartTime2 = new Date().getTime();
-  //             db.query({map: map},function (err, response) {
-  //               ok(!err,'no errors');
-  //               var queryDuration2 = new Date().getTime() - queryStartTime2;
-  //               console.log("write doc1/string1 took " + writeDuration1 + " ms");
-  //               console.log("write doc1/buffer1 took " + writeDuration2 + " ms");
-  //               console.log("query before attachments took " + queryDuration1 + " ms");
-  //               console.log("query after attachments took " + queryDuration2 + " ms");
-  //               ok(queryDuration2 <= queryDuration1 * 10, 'Query finished within order of magnitude');
-  //               done();
-  //             });
-  //           });
-  //         });
-  //       });
-  //     });
-  //   });
-  // });
-
-});
+  })
+})
